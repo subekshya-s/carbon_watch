67e0295da483c0fbd073a2a40a52293f80157487
subekshya-s <subekshyasubedi26@gmail.com>
2026-06-29 04:31:55 +0000

    feat: load Nepal 77 districts from GeoJSON (closes #4)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -87,10 +87,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+from decouple import config
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'carbonwatch_db',
+        'USER': 'carbonwatch_user',
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
