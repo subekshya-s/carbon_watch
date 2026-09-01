5eaa56bfd467c70208528d5aeac95a707609983d
subekshya-s <subekshyasubedi26@gmail.com>
2026-09-01 06:37:40 +0000

    Update API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://carbon-watch-jgky.onrender.com/api',
 })
 
 export const getDistricts = () => API.get('/districts/')
